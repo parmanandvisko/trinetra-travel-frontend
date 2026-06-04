124cbc207f669af7bd64379c1fcb8d31f2292fb5
priyanshu <parmanand.visko@gmail.com>
2026-06-04 16:43:54 +0530

    change url build

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BACKEND_URL ='https://api.trinetraglobalholidays.com/api'
+const BACKEND_URL ='https://api.trinetraglobalholidays.com/api' 
 
 const api = axios.create({
   baseURL: BACKEND_URL,

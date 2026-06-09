d4f811abe48c74454a52da8d0ecf06edad3cb1a3
priyanshu <parmanand.visko@gmail.com>
2026-06-09 12:36:49 +0530

    add new data

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // const BACKEND_URL ='https://api.trinetraglobalholidays.com/api' 
 const BACKEND_URL ='http://localhost:5012/api' 
 
-const api = axios.create({
+const api = axios.create({ 
   baseURL: BACKEND_URL,
   timeout: 15000,
 })
